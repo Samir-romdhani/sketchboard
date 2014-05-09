edf3d5e7772118b7d155e6022ad731e1612a5494
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-05-09 14:23:48 +0600

    Add partials

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@
 
 var express = require('express');
 var morgan  = require('morgan');
+var hbs = require('hbs');
 
 var app = express();
 app.engine('hbs', require('hbs').__express);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(__dirname + '/../public'));
 
