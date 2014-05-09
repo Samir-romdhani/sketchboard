e0ed0083b3d3fc82889ae3ee4ba1a32aa3f5329f
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-05-09 15:40:31 +0600

    Add bump and publish tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var source = require('vinyl-source-stream');
 var spawn = require('child_process').spawn;
 var rimraf = require('rimraf');
 var runSequence = require('run-sequence');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
 
 gulp.task('browserify', function() {
     return browserify(__dirname + '/js/index.js')
@@ -15,6 +17,12 @@ gulp.task('browserify', function() {
 });
 
 gulp.task('build', ['browserify']);
+
+gulp.task('bump', function () {
+    gulp.src('./*.json')
+        .pipe(bump({ type:'patch' }))
+        .pipe(gulp.dest('./'));
+});
 
 gulp.task('jitsu', function (cb) {
   var jitsu = spawn('jitsu', ['deploy'], { stdio: 'inherit' });
@@ -27,6 +35,15 @@ gulp.task('clean', function (cb) {
     rimraf(__dirname + '/public/js', cb);
 });
 
+gulp.task('publish', function(){
+    var version = require('./package.json').version;
+    return git
+        .commit('v' + version, { args: '-A' })
+        .tag('v' + version, 'Releasing v' + version + ' version')
+        .push('origin', 'master')
+        .end();
+});
+
 gulp.task('deploy', function (cb) {
-    runSequence('build', 'jitsu', 'clean', cb);
+    runSequence('build', 'bump', 'jitsu', 'clean', 'push', cb);
 });
