8ffaee3afae4d11f4134100d3d4dd94d7b028241
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-05-09 15:25:15 +0600

    Add deploy task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
+var spawn = require('child_process').spawn;
+var rimraf = require('rimraf'); // rimraf directly
 
 gulp.task('browserify', function() {
     return browserify(__dirname + '/js/index.js')
@@ -12,3 +14,16 @@ gulp.task('browserify', function() {
 });
 
 gulp.task('build', ['browserify']);
+
+gulp.task('jitsu', function (cb) {
+  var jitsu = spawn('jitsu', ['deploy'], { stdio: 'inherit' });
+  jitsu.on('close', function (code) {
+    cb(code !== 0);
+  });
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(__dirname + '/public/js', cb);
+});
+
+gulp.task('deploy', ['build', 'jitsu', 'clean']);
