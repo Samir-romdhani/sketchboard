dea1fa0dce9f43ad219f757d3e01e7170e7dcaa8
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-05-16 10:37:48 +0600

    Track quadrant

--- js/components/stage.js
+++ js/components/stage.js
@@ -9,6 +9,18 @@ var Colorsbar = require('./colorsbar.js');
 var _ = require('lodash');
 var Emitter = require('wildemitter');
 
+var quadrant = {
+    x: 0x0BAD,
+    y: 0xFACE,
+    toString: function () {
+        return this.x.toString(16) + " : " + this.y.toString(16);
+    },
+    update: function (p) {
+        this.x = Math.floor(p.x / 100);
+        this.y = Math.floor(p.y / 100);
+    }
+}
+
 var Stage = function () {
     PIXI.Stage.call(this, 0xFFFFFF, true);
 
@@ -17,12 +29,17 @@ var Stage = function () {
 
     var docDrag = new DOCDrag(bg);
 
+    this.quadrant = quadrant;
+
     docDrag.on('drag', function (d) {
         if (!moving) { return; }
         bg.tilePosition.x -= d.x;
         bg.tilePosition.y -= d.y;
         graphics.position.x -= d.x;
         graphics.position.y -= d.y;
+
+        quadrant.update(graphics.position);
+        quadrantText.setText(quadrant.toString());
     });
 
     docDrag.on('line', function (a, b) {
@@ -36,12 +53,14 @@ var Stage = function () {
     this.addChild(docDrag);
 
     var graphics = new PIXI.Graphics();
+    graphics.position.x = quadrant.x * 100;
+    graphics.position.y = quadrant.y * 100;
     this.addChild(graphics);
 
-    var text = new PIXI.Text('0 : 0', { font: '12px verdana', fill: 'black' });
-    text.position.x = 20;
-    text.position.y = 20;
-    this.addChild(text);
+    var quadrantText = new PIXI.Text(quadrant.toString(), { font: '12px verdana', fill: 'black' });
+    quadrantText.position.x = 20;
+    quadrantText.position.y = 20;
+    this.addChild(quadrantText);
 
     var sidebar = new Sidebar();
     sidebar.position.x = 20;
